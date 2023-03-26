006a39b0707e1cf868fdb1f8219e830233b2fb40
EduBaltazar <edwbaltazar@gmail.com>
2023-03-26 01:32:24 -0300

    up

---
+++ src/app/components/hyskills/hyskills.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HyskillsComponent } from './hyskills.component';
+
+describe('HyskillsComponent', () => {
+  let component: HyskillsComponent;
+  let fixture: ComponentFixture<HyskillsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HyskillsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HyskillsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
